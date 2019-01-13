cd511ba2657d21766b911e6fe3762146d99f83e6
Alexander Sanakoev <asanakoev@blocshop.cz>
2019-01-13 20:53:28 +0400

    favorite component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { CoursesService } from './courses/courses.service';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.scss']
+})
+export class FavoriteComponent implements OnInit {
+  isFavorite: boolean;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onStarClicked(){
+    this.isFavorite = !this.isFavorite;
+  }
+}
