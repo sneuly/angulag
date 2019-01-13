8c85f616317ea1b21d726803cba98fe1941adf1f
Alexander Sanakoev <asanakoev@blocshop.cz>
2019-01-13 21:47:00 +0400

    added hover

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from "@angular/core";
 export class FavoriteComponent implements OnInit {
   isFavorite: boolean;
   rating: number = 0;
+  savedRating: number;
   stars = [1, 2, 3, 4, 5];
 
   constructor() {}
@@ -15,7 +16,22 @@ export class FavoriteComponent implements OnInit {
   ngOnInit() {}
 
   onStarClicked(rating: number) {
+    if (rating === this.savedRating) {
+      this.rating = 0;
+      this.savedRating = 0;
+    } else {
+      this.rating = rating;
+      this.savedRating = rating;
+    }
+  }
+
+  onStarMouseEnter(rating: number) {
+    this.savedRating = this.rating;
     this.rating = rating;
+  }
+
+  onStarMouseLeave() {
+    this.rating = this.savedRating;
   }
 
   isRated(starOrder: number) {
